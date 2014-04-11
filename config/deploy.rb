2f7c64ff6f3089f7a6eadaca50a9ef8db9c80055
William Hurley <whurley@forumone.com>
2014-04-11 14:58:25 -0400

    Adding comments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,37 @@
+# The name for the application, should only be things that can be in a directory name
 set :application, 'app'
+
+# The repo URL
 set :repo_url, 'repo'
 
+# Use git for the SCM
 set :scm, :git
+
+# Use a remote cache for git
 set :deploy_via, :remote_cache
 
+# Which logging formatter to use
 set :format, :pretty
+
+# Which logging level to use
 set :log_level, :debug
+
+# Whether to use sudo for commands
 set :user_sudo, false
+
+# Whether to use a pseudo-TTY
 set :pty, true
 
+# Array of folders to share
 set :linked_dirs, %w{public/sites/default/files}
 
+# Number of release directories to keep
 set :keep_releases, 3
+
+# Add custom SSH config
+set :ssh_options, {
+  config: 'config/ssh_config'
+}
 
 after "deploy:publishing", "deploy:restart"
 after "deploy:symlink:release", "deploy:symlink:drupal"
