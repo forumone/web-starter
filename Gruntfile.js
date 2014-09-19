7a873c46ee894e09dd182ab008ebdbb3ddd746d1
William Hurley <whurley@forumone.com>
2014-09-18 20:40:48 -0400

    Adding gems

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+// Gruntfile.js
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve --baseurl=""',
+                options: {
+                	stderr: false,
+                	stdout: false
+                }
+            }
+        },
+        watch: {
+            files: [
+                '_includes/*.html',
+                '_layouts/*.html',
+                '_posts/*.markdown',
+                '_config.yml',
+                'index.html',
+                'localdev/*.html'
+            ],
+            tasks: ['shell:jekyllBuild', 'shell:jekyllServe'],
+            options: {
+                interrupt: true,
+                atBegin: true,
+                livereload: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['shell']);
+};
