24bccd138b7d7228d03d35b2c571a057bd26a361
Campbell Vertesi <campbell@vertesi.com>
2015-05-26 20:16:05 +0200

    Create a self-signed cert before provision
    There's a separate update on forumone-puppet that adds HTTPS by default
    for Apache and NGINX. NGINX (and puppet in general) lacks the ability to
    generate certs for itself, so we have to do it here. This step is
    redundant when hosting on Apache, or using the box without a web server.

--- puppet/shell/pre-provision.sh
+++ puppet/shell/pre-provision.sh
@@ -2,6 +2,11 @@
 
 VAGRANT_CORE_FOLDER="/vagrant"
 
+# Create default self-signed SSL cert.
+if [[ ! -f "/etc/pki/tls/private/localhost.key" ]]; then
+    /usr/bin/openssl req -newkey rsa:2048 -nodes -keyout /etc/pki/tls/private/localhost.key  -x509 -days 365 -out /etc/pki/tls/certs/localhost.crt -subj '/CN=localhost.localdomain' > /dev/null
+fi
+
 if [[ -f "${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh" ]]; then
   source ${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh
 fi;
