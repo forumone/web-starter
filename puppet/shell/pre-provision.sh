ff4116e0024e11eae9d3b9807108e45ec3998f11
Mike Shade <mshade@mshade.org>
2015-08-18 21:29:08 -0400

    remove puppet stuff from salt branch

--- puppet/shell/pre-provision.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VAGRANT_CORE_FOLDER="/vagrant"
-
-if [[ -f "${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh" ]]; then
-  source ${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh
-fi;
-
