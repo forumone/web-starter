2c625d051d21d21dce3a5f420313eab1e04139e5
Daniel McDermott <dmcdermott@forumone.com>
2016-04-05 11:16:20 -0700

    Moved configuration per https://github.com/forumone/web-starter/pull/180#discussion_r58574496

--- tasks/config/behat.js
+++ tasks/config/behat.js
@@ -1,8 +1,45 @@
 module.exports = function (grunt) {
     grunt.config.merge({
-        behat: {
+        behatLocal: {
             src: './tests/behat/features/***/**/*',
             options: {
+                config: './tests/behat/behat.yml',
+                maxProcesses: 5,
+                bin: './tests/behat/bin/behat',
+                junit: {
+                    output_folder: 'tests/test_results/'
+                }
+            }
+        },
+        behatLocalSelenium: {
+            src: './tests/behat/features/***/**/*',
+            options: {
+                config: './tests/behat/behat.yml',
+                flags: '-p local-selenium',
+                maxProcesses: 5,
+                bin: './tests/behat/bin/behat',
+                junit: {
+                    output_folder: 'tests/test_results/'
+                }
+            }
+        },
+        behatJenkinsDev: {
+            src: './tests/behat/features/***/**/*',
+            options: {
+                config: './tests/behat/behat.jenkins.yml',
+                flags: '-p dev',
+                maxProcesses: 5,
+                bin: './tests/behat/bin/behat',
+                junit: {
+                    output_folder: 'tests/test_results/'
+                }
+            }
+        },
+        behatJenkinsStage: {
+            src: './tests/behat/features/***/**/*',
+            options: {
+                config: './tests/behat/behat.jenkins.yml',
+                flags: '-p stage',
                 maxProcesses: 5,
                 bin: './tests/behat/bin/behat',
                 junit: {
--- tasks/register/behatRegister.js
+++ tasks/register/behatRegister.js
@@ -3,11 +3,7 @@ module.exports = function (grunt) {
     // Point to local Behat default Profile
     grunt.registerTask('behatLocal', 'Execute Behat for BDD testing', function () {
         grunt.config.merge({
-            behat: {
-                options: {
-                    config: './tests/behat/behat.yml'
-                }
-            }
+            behat: grunt.config.data.behatLocal
         });
         grunt.task.run('behat')
     });
@@ -15,12 +11,7 @@ module.exports = function (grunt) {
     // Point to local Behat Selenium
     grunt.registerTask('behatLocalSelenium', 'Execute Behat for BDD testing', function () {
         grunt.config.merge({
-            behat: {
-                options: {
-                    config: './tests/behat/behat.yml',
-                    flags: '-p local-selenium'
-                }
-            }
+            behat: grunt.config.data.behatLocalSelenium
         });
         grunt.task.run('behat')
     });
@@ -28,12 +19,7 @@ module.exports = function (grunt) {
     // Point to Jenkins Dev environment
     grunt.registerTask('behatJenkinsDev', 'Execute Behat for BDD testing', function () {
         grunt.config.merge({
-            behat: {
-                options: {
-                    config: './tests/behat/behat.jenkins.yml',
-                    flags: '-p dev'
-                }
-            }
+            behat: grunt.config.data.behatJenkinsDev
         });
         grunt.task.run('behat')
     });
@@ -41,12 +27,7 @@ module.exports = function (grunt) {
     // Point to Jenkins Stage environment
     grunt.registerTask('behatJenkinsStage', 'Execute Behat for BDD testing', function () {
         grunt.config.merge({
-            behat: {
-                options: {
-                    config: './tests/behat/behat.jenkins.yml',
-                    flags: '-p stage'
-                }
-            }
+            behat: grunt.config.data.behatJenkinsStage
         });
         grunt.task.run('behat')
     });
