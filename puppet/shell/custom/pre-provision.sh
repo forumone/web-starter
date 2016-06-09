c5c3f098e5fca9859b6df15cb1f6dd8de84b051e
Mike Shade <mshade@mshade.org>
2016-06-08 22:24:33 -0400

    update for use with bento box

--- puppet/shell/custom/pre-provision.sh
+++ puppet/shell/custom/pre-provision.sh
@@ -1,12 +1,8 @@
-# Guest additions for vboxsf, requisites for salt bootstrap
-yum upgrade nss ca-certificates python -y
-yum install git -y
-#yum upgrade python-jinja2-26 git nss ca-certificates --enablerepo=epel -y
-
 echo 'export PYTHONPATH=/usr/lib/python2.6/site-packages/Jinja2-2.6-py2.6.egg:$PYTHONPATH' > /etc/profile.d/salt-jinja.sh
 chmod 755 /etc/profile.d/salt-jinja.sh
 
 # Pre-fetch git formulas
+yum install git -y
 cd /srv/salt/formulas
 for repo in $(grep github.com /vagrant/salt/minion |awk '{print $2}' |sed -e 's/:$//')
 do
