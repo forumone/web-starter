7366751d06a25023054cc727257766bfd5de22da
Mike Shade <mshade@forumone.com>
2017-05-18 10:54:57 -0400

    fix path of pre-provision script from /puppet to /config/shell/custom

--- config/shell/pre-provision.sh
+++ config/shell/pre-provision.sh
@@ -5,7 +5,7 @@ VAGRANT_CORE_FOLDER="/vagrant"
 # Add hostname to /etc/hosts for ipv6. Vagrant doesn't handle natively
 sed -i '/^::1 / s/$/ vagrant.byf1.io/' /etc/hosts
 
-if [[ -f "${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh" ]]; then
-  source ${VAGRANT_CORE_FOLDER}/puppet/shell/custom/pre-provision.sh
+if [[ -f "${VAGRANT_CORE_FOLDER}/config/shell/custom/pre-provision.sh" ]]; then
+  source ${VAGRANT_CORE_FOLDER}/config/shell/custom/pre-provision.sh
 fi;
 
